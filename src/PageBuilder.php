fd2a53caaea8de5f2e997b081b628fbd0f60fa63
Sybren Dijkstra <nerbiz@gmail.com>
2017-05-05 21:56:50 +0200

    Renamed PageBuilder::addRowStyle() to addRowStyles()

--- src/PageBuilder.php
+++ src/PageBuilder.php
@@ -124,7 +124,7 @@ class PageBuilder {
      *                                  array: property:value pairs
      * @param  String|null   $value     In case $property is a string, this is the value for it
      */
-    public static function addRowStyle($property, $value=null) {
+    public static function addRowStyles($property, $value=null) {
         if(is_string($property))
             $style = $property . ': ' . $value . ';';
 
