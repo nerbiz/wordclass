3f28471588b37d02057e369732f15ede263327e3
Sybren Dijkstra <nerbiz@gmail.com>
2017-05-13 01:26:09 +0200

    Updated Plugins + docs, and added release notes

--- docs/classes/Plugins.md
+++ docs/classes/Plugins.md
@@ -1,13 +1,10 @@
 # Wordclass\Plugins
 Depends on [tgmpa/tgm-plugin-activation](https://github.com/TGMPA/TGM-Plugin-Activation).  
-See [see documentation](http://tgmpluginactivation.com/configuration/) for more information.  
-Set required/recommended plugins, using a method chain.  
-
-### ::init()
-Start the method chain.
+See the [documentation](http://tgmpluginactivation.com/configuration/) for more information.  
+Set required/recommended plugins.
 
 ### ::config()
-Overwrite the default config.
+Overwrite the default config. This is optional, but if set, it needs to be done before including plugins.
 
 ### ::include()
 Require or recommend 1 or more pugins, in slug:options pairs.  
@@ -15,28 +12,32 @@ To add only 1, it is possible to provide a string (slug) and the options as a se
 
 #### Example
 ```php
-// One plugin, with default config
-Wordclass\Plugins::init()
-    ->include('all-in-one-wp-migration', [
-        'name'      => 'All-in-One WP Migration',
-        'required'  => false
-    ])
-    ->set();
+// 1 config value overwritten
+Wordclass\Plugins::config([
+    'dismissable' => false
+]);
 
-// Multiple plugins
-Wordclass\Plugins::init()
-    ->config([
-        'dismissable' => false
-    ])
-    ->include([
-        'all-in-one-wp-migration' => [
-            'name'      => 'All-in-One WP Migration',
-            'required'  => false
-        ],
-        'disable-feeds' => [
-            'name'      => 'Disable Feeds',
-            'required'  => false
-        ]
-    ])
-    ->set();
+// One plugin
+Wordclass\Plugins::include('all-in-one-wp-migration', [
+    'name'     => 'All-in-One WP Migration',
+    'required' => false
+]);
+
+// Another one can be added later
+Wordclass\Plugins::include('disable-feeds', [
+    'name'     => 'Disable Feeds',
+    'required' => false
+]);
+
+// Multiple plugins, at the same time (same result as the above)
+Wordclass\Plugins::include([
+    'all-in-one-wp-migration' => [
+        'name'     => 'All-in-One WP Migration',
+        'required' => false
+    ],
+    'disable-feeds' => [
+        'name'     => 'Disable Feeds',
+        'required' => false
+    ]
+]);
 ```
--- src/Plugins.php
+++ src/Plugins.php
@@ -3,29 +3,20 @@
 namespace Wordclass;
 
 class Plugins {
-    private $_id;
-    private $_includePlugins = false;
-    private $_config = false;
-
-
-
-    public function __construct($id) {
-        $this->_id = $id;
-    }
+    private static $_config = [];
 
 
 
     /**
      * Set the config for TGMPA
      * @param   Array  $config  Values that will overwrite the defaults
-     * @return  $this
      */
-    public function config($config=[]) {
+    public static function config($config=[]) {
         $config = (array) $config;
 
-        $this->_config = array_replace_recursive([
+        static::$_config = array_replace_recursive([
             // Unique ID for hashing notices for multiple instances of TGMPA.
-            'id'           => $this->_id,
+            'id'           => uniqid('', true),
             // Default absolute path to bundled plugins.
             'default_path' => '',
             // Menu slug.
@@ -45,8 +36,6 @@ class Plugins {
             // Message to output right before the plugins table.
             'message'      => ''
         ], $config);
-
-        return $this;
     }
 
 
@@ -56,45 +45,24 @@ class Plugins {
      * @param   Array       $plugins  An array of slug:options pairs
      *                                  Can be a string (slug), but then the 2nd parameter is required
      * @param   Array|null  $options  If $plugins is a string (slug), these are the options for it
-     * @return  $this
      */
-    public function include($plugins, $options=null) {
-        $this->_includePlugins = [];
-
+    public static function include($plugins, $options=null) {
         if(is_string($plugins))
             $plugins = [$plugins => $options];
 
-        foreach($plugins as $slug => $options) {
-            $options['slug'] = $slug;
-            $this->_includePlugins[] = $options;
-        }
+        // Use the default config, if it's not set yet
+        if(empty(static::$_config))
+            static::config();
 
-        return $this;
-    }
+        add_action('tgmpa_register', function() use($plugins) {
+            $includePlugins = [];
 
+            foreach($plugins as $slug => $options) {
+                $options['slug'] = $slug;
+                $includePlugins[] = $options;
+            }
 
-
-    /**
-     * Apply the plugins and settings
-     */
-    public function set() {
-        if($this->_includePlugins) {
-            if( ! $this->_config)
-                $this->config();
-
-            add_action('tgmpa_register', function() {
-                tgmpa($this->_includePlugins, $this->_config);
-            });
-        }
-    }
-
-
-
-    /**
-     * Create a new TGMPA instance
-     * @return Object  An instance of this class
-     */
-    public static function init() {
-        return new static(uniqid('', true));
+            tgmpa($includePlugins, static::$_config);
+        });
     }
 }
