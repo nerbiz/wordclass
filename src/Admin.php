57b21e08a2641cf125f414c4b4ac91339a02e114
Sybren Dijkstra <nerbiz@gmail.com>
2017-05-13 19:41:29 +0200

    Admin::roleRedirects() now implicitly makes relative URLs when needed

--- src/Admin.php
+++ src/Admin.php
@@ -24,13 +24,17 @@ class Admin {
             // If role(s) are set, see if it matches the given role(s), then redirect to the corresponding URL
             if(isset($user->roles)  &&  is_array($user->roles)) {
                 foreach($user->roles as $role) {
-                    if(is_string($roleUrls)  &&  $role == $roleUrls)
-                        return esc_url($url);
+                    // Get the URL from the given role:url array
+                    // When both arguments are a string, the second one is the $url variable already
+                    if(is_array($roleUrls)  &&  array_key_exists($role, $roleUrls))
+                        $url = $roleUrls[$role];
 
-                    else if(is_array($roleUrls)) {
-                        if(array_key_exists($role, $roleUrls))
-                            return esc_url($roleUrls[$role]);
-                    }
+                    // Literal URLs
+                    if(preg_match('~^https?://~', $url))
+                        return esc_url($url);
+                    // Other paths are relative to the home URL
+                    else
+                        return esc_url(home_url() . '/' . $url);
                 }
             }
 
