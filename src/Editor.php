f8f694e505b305d2f2d60595643cdca0b093c2e5
Sybren Dijkstra <nerbiz@gmail.com>
2017-09-15 00:42:55 +0200

    Added Editor::addPlugin() to easily add a TinyMCE plugin

--- src/Editor.php
+++ src/Editor.php
@@ -21,16 +21,35 @@ class Editor {
 
 
     /**
+     * Force the advanced toolbar (2nd row of buttons) of the TinyMCE editor
+     * @param  Boolean  $keepButton  (Optional) keep the toggle button
+     */
+    public static function forceAdvanced($keepButton=false) {
+        // Remove the toggle button
+        if(is_bool($keepButton)  &&  ! $keepButton)
+            static::removeButton(1, 'wp_adv');
+
+        // Force the 2nd buttons row
+        add_filter('tiny_mce_before_init', function($args) {
+            $args['wordpress_adv_hidden'] = false;
+            return $args;
+        });
+    }
+
+
+
+    /**
      * Add a button to the TinyMCE editor
      * @param  Integer  $toolbar  The toolbar number, 1 = default, 2 = advanced
      * @param  String   $name     The name of the button
      * @param  String   $after    (Optional) the name of the button to place the new button after
      *                            'first' places the button as the first one
+     *                            null places the button at the end
      */
     public static function addButton($toolbar, $name, $after=null) {
         $filter = static::getButtonsFilter($toolbar);
 
-        add_filter($filter, function($buttons) use ($name, $after) {
+        add_filter($filter, function($buttons) use($name, $after) {
             // Append the button at the end, if 'after' is not specified
             if($after == null)
                 $buttons[] = $name;
@@ -65,7 +84,7 @@ class Editor {
     public static function removeButton($toolbar, $name, $replacement=null) {
         $filter = static::getButtonsFilter($toolbar);
 
-        add_filter($filter, function($buttons) use ($name, $replacement) {
+        add_filter($filter, function($buttons) use($name, $replacement) {
             $removeButtonKey = array_search($name, $buttons);
 
             // Only remove/replace the button if it exists
@@ -109,18 +128,27 @@ class Editor {
 
 
     /**
-     * Force the advanced toolbar (2nd row of buttons) of the TinyMCE editor
-     * @param  Boolean  $keepButton  (Optional) keep the toggle button
+     * Add a TinyMCE plugin to the editor
+     * @param  String   $name     The name of the plugin
+     * @param  Integer  $toolbar  (Optional) the toolbar number, 1 = default, 2 = advanced, or false to not add
+     * @param  String   $after    (Optional) the name of the button to place the new button after
+     *                            'first' places the button as the first one
+     *                            null places the button at the end
      */
-    public static function forceAdvanced($keepButton=false) {
-        // Remove the toggle button
-        if(is_bool($keepButton)  &&  ! $keepButton)
-            static::removeButton(1, 'wp_adv');
+    public static function addPlugin($name, $toolbar=1, $after=null) {
+        $pluginPath = 'tinymce/tinymce/plugins/' . $name . '/plugin.min.js';
 
-        // Force the 2nd buttons row
-        add_filter('tiny_mce_before_init', function($args) {
-            $args['wordpress_adv_hidden'] = false;
-            return $args;
-        });
+        // The plugin needs to exist
+        if(is_readable(__DIR__ . '/../../../' . $pluginPath)) {
+            // Add the plugin
+            add_filter('mce_external_plugins', function($plugins) use($name, $toolbar, $after, $pluginPath) {
+                $plugins[$name] = get_stylesheet_directory_uri() . '/vendor/' . $pluginPath;
+                return $plugins;
+            });
+
+            // Add a button if needed
+            if($toolbar != false)
+                static::addButton($toolbar, $name, $after);
+        }
     }
 }
