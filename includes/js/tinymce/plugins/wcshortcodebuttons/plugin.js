61858ad1ea3ee06ce2089c01e03e2578445446c9
Sybren Dijkstra <nerbiz@gmail.com>
2017-09-29 17:12:41 +0200

    Fixed: all shortcode buttons used the modal of the last one

--- includes/js/tinymce/plugins/wcshortcodebuttons/plugin.js
+++ includes/js/tinymce/plugins/wcshortcodebuttons/plugin.js
@@ -2,51 +2,46 @@
 if(window.wordclassShortcodeButtons) {
     tinymce.create('tinymce.plugins.WordclassShortcodeButtons', {
         init: function(editor, url) {
-            var shortcode = null;
-
             // Create all the shortcode buttons
             for(var i=-1;  ++i<window.wordclassShortcodeButtons.length;) {
-                // For convenience
-                shortcode = window.wordclassShortcodeButtons[i];
+                // Use an IIFE, so that values are scoped
+                (function(editor, url, shortcode) {
+                    // Create and register the button
+                    editor.addButton(shortcode.id, {
+                        text: shortcode.buttontext,
+                        icon: 'code',
+                        onclick: function() {
+                            // If text is selected in the editor, store it for if the tag is enclosing
+                            var selectedText = tinyMCE.activeEditor.selection.getContent();
 
-                // Create and register the button
-                editor.addButton(shortcode.id, {
-                    text: shortcode.buttontext,
-                    icon: 'code',
-                    cmd: shortcode.id
-                });
+                            editor.windowManager.open({
+                                title: shortcode.buttontext,
+                                body: shortcode.inputs,
+                                // Insert the shortcode when the window form is submitted
+                                onsubmit: function(event) {
+                                    // Put the shortcode parameters in an array ('name="value"' strings)
+                                    // Exclude empty values
+                                    var params = [];
+                                    for(var name in event.data) {
+                                        if(event.data[name] != '')
+                                            params.push(name + '="' + event.data[name] + '"');
+                                    }
 
-                // Define the action for the button
-                editor.addCommand(shortcode.id, function() {
-                    // If text is selected in the editor, store it for if the tag is enclosing
-                    var selectedText = tinyMCE.activeEditor.selection.getContent();
+                                    // Construct and insert the shortcode
+                                    var tag = '[' + shortcode.tag + ' ' + params.join(' ') + ']';
+                                    // Enclose the selected text in the shortcode
+                                    if(shortcode.enclosing)
+                                        tag += selectedText + '[/' + shortcode.tag + '] ';
+                                    // Otherwise put the shortcode before the selected text, to not lose text
+                                    else
+                                        tag += ' ' + selectedText;
 
-                    editor.windowManager.open({
-                        title: shortcode.buttontext,
-                        body: shortcode.inputs,
-                        // Insert the shortcode when the window form is submitted
-                        onsubmit: function(event) {
-                            // Put the shortcode parameters in an array ('name="value"' strings)
-                            // Exclude empty values
-                            var params = [];
-                            for(var name in event.data) {
-                                if(event.data[name] != '')
-                                    params.push(name + '="' + event.data[name] + '"');
-                            }
-
-                            // Construct and insert the shortcode
-                            var tag = '[' + shortcode.tag + ' ' + params.join(' ') + ']';
-                            // Enclose the selected text in the shortcode
-                            if(shortcode.enclosing)
-                                tag += selectedText + '[/' + shortcode.tag + '] ';
-                            // Otherwise put the shortcode before the selected text, to not lose text
-                            else
-                                tag += ' ' + selectedText;
-
-                            editor.insertContent(tag);
+                                    editor.insertContent(tag);
+                                }
+                            });
                         }
                     });
-                });
+                })(editor, url, window.wordclassShortcodeButtons[i]);
             }
         }
     });
