a5a213fbf4f1edc9046580ed4726b81c6cb676e4
Sybren Dijkstra <nerbiz@gmail.com>
2018-10-28 00:23:29 +0200

    Added a Factory, in preparation for removing static usage

---
+++ src/Factory.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Nerbiz\Wordclass;
+
+use ReflectionClass;
+use Exception;
+
+class Factory
+{
+    /**
+     * Create and return a new instance
+     * @param  string $classname The name of the class to initiate
+     * @param  array  $arguments Constructor arguments for the class
+     * @return object
+     * @throws Exception If the class is not found
+     */
+    public function make($classname, $arguments = [])
+    {
+        // Prepend the namespace to the classname if needed
+        if (strpos($classname, __NAMESPACE__) === false) {
+            $fullyQualifiedClassname = __NAMESPACE__ . '\\' . $classname;
+        } else {
+            $fullyQualifiedClassname = $classname;
+        }
+
+        if (class_exists($fullyQualifiedClassname)) {
+            $reflection = new ReflectionClass($fullyQualifiedClassname);
+            return $reflection->newInstanceArgs($arguments);
+        }
+
+        throw new Exception(sprintf(
+            "%s(): the class '%s' doesn't exist",
+            __METHOD__,
+            $fullyQualifiedClassname
+        ));
+    }
+}
