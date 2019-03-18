b194f95aa37f6b5ff513882bfce2d1ff249be94e
Sybren Dijkstra <nerbiz@gmail.com>
2019-03-18 12:27:34 +0100

    Added a 'clear' button to media upload elements + improved the script

--- includes/js/media-upload.js
+++ includes/js/media-upload.js
@@ -1,62 +1,146 @@
-jQuery(document).ready(function ($) {
-    var fileFrame;
+/**
+ * @param {jQuery|HTMLElement} $element
+ * @constructor
+ */
+function MediaUploadInput($element)
+{
+    var self = this;
 
-    // Store the old id
-    var oldAttachmentId = (wp.media)
-        ? wp.media.model.settings.post.id
-        : null;
+    /**
+     * The main element containing image preview and input
+     * @type {jQuery|HTMLElement}
+     */
+    self.$element = $($element);
 
-    $('.upload-media-button').on('click', function (event) {
-        event.preventDefault();
+    /**
+     * The button that triggers the media selector
+     * @type {jQuery|HTMLElement}
+     */
+    self.$uploadButton = self.$element.find('.upload-media-button');
 
-        // Get the current attachment ID
-        var $button = $(event.target);
-        var currentAttachmentId = $button.data('attachmentId');
+    /**
+     * The button that clears the current value
+     * @type {jQuery|HTMLElement}
+     */
+    self.$clearButton = self.$element.find('.clear-media-button');
 
-        // If the media frame already exists, reopen it.
-        if (fileFrame) {
-            // Set the post ID to what we want
-            fileFrame.uploader.uploader.param('post_id', currentAttachmentId);
-            // Open frame
-            fileFrame.open();
-            return;
-        } else {
-            // Set the wp.media post id so the uploader grabs the ID we want when initialised
-            wp.media.model.settings.post.id = currentAttachmentId;
+    /**
+     * The image element that shows the selected attachment
+     * @type {jQuery|HTMLElement}
+     */
+    self.$imagePreview = self.$element.find('.image-preview');
+
+    /**
+     * The input field containing the value (selected attachment ID)
+     * @type {jQuery|HTMLElement}
+     */
+    self.$inputField = self.$element.find('input[type="hidden"]');
+
+    /**
+     * A 1x1 transparent white pixel, used as image placeholder
+     * @type {String}
+     */
+    self.transparentPixelBase64 = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=';
+
+    /**
+     * The media frame
+     * @type {Object|null}
+     */
+    self.fileFrame = null;
+
+    /**
+     * Set a fallback 'src' for the preview image, if needed
+     * @return {void}
+     */
+    self.setFallbackImagePreview = function () {
+        if (! self.$imagePreview.attr('src')) {
+            self.$imagePreview.attr('src', self.transparentPixelBase64);
         }
+    };
 
-        // Create the media frame
-        fileFrame = wp.media.frames.file_frame = wp.media({
-            title: 'Select a image to upload',
-            button: {
-                text: 'Use this image',
-            },
-            // Don't allow multiple files
-            multiple: false
+    /**
+     * Enable the media upload/selector button
+     * @return {void}
+     */
+    self.enableUploadButton = function () {
+        self.$uploadButton.on('click', function (event) {
+            event.preventDefault();
+
+            // Get the current attachment ID
+            var currentAttachmentId = self.$inputField.val();
+
+            // If the media frame already exists, reopen it.
+            if (self.fileFrame) {
+                // Set the post ID to what we want
+                self.fileFrame.uploader.uploader.param('post_id', currentAttachmentId);
+                // Open frame
+                self.fileFrame.open();
+                return;
+            } else {
+                // Set the wp.media post id so the uploader grabs the ID we want when initialised
+                wp.media.model.settings.post.id = currentAttachmentId;
+            }
+
+            // Create the media frame
+            self.fileFrame = wp.media.frames.file_frame = wp.media({
+                title: 'Select a image to upload',
+                button: {
+                    text: 'Use this image',
+                },
+                // Don't allow multiple files
+                multiple: false
+            });
+
+            // When an image is selected, run a callback.
+            self.fileFrame.on('select', function () {
+                // We set multiple to false so only get one image from the uploader
+                var attachment = self.fileFrame.state().get('selection').first().toJSON();
+
+                // Show the image preview
+                self.$imagePreview.attr('src', attachment.url);
+                // Set the attachment ID in the input field
+                self.$inputField.val(attachment.id);
+
+                // Restore the main post ID
+                wp.media.model.settings.post.id = window.mediaUploadInputSettings.oldAttachmentId;
+            });
+
+            // Finally, open the modal
+            self.fileFrame.open();
         });
+    };
 
-        // When an image is selected, run a callback.
-        fileFrame.on('select', function () {
-            // We set multiple to false so only get one image from the uploader
-            var attachment = fileFrame.state().get('selection').first().toJSON();
+    /**
+     * Enable the button that clears the current value
+     * @return {void}
+     */
+    self.enableClearButton = function () {
+        self.$clearButton.on('click', function (event) {
+            event.preventDefault();
 
-            // Show the image preview
-            $button.prev('.image-preview-wrapper')
-                .find('.image-preview')
-                .attr('src', attachment.url);
-            // Set the attachment ID in the input field
-            $button.next('input[type="hidden"]').val(attachment.id);
-
-            // Restore the main post ID
-            wp.media.model.settings.post.id = oldAttachmentId;
+            self.$imagePreview.attr('src', self.transparentPixelBase64);
+            self.$inputField.val('');
         });
+    };
+}
 
-        // Finally, open the modal
-        fileFrame.open();
+jQuery(document).ready(function ($) {
+    window.mediaUploadInputSettings = {
+        // Store the old id
+        oldAttachmentId: (wp.media)
+            ? wp.media.model.settings.post.id
+            : null
+    };
+
+    $('.media-upload-input').each(function (index, element) {
+        var mediaUploadInput = new MediaUploadInput(element);
+        mediaUploadInput.setFallbackImagePreview();
+        mediaUploadInput.enableUploadButton();
+        mediaUploadInput.enableClearButton();
     });
 
     // Restore the main ID when the add media button is pressed
     $('.add_media').on('click', function () {
-        wp.media.model.settings.post.id = oldAttachmentId;
+        wp.media.model.settings.post.id = window.mediaUploadInputSettings.oldAttachmentId;
     });
 });
--- src/SettingsPage.php
+++ src/SettingsPage.php
@@ -181,14 +181,17 @@ class SettingsPage
         $currentMediaUrl = trim(wp_get_attachment_image_url(get_option($arguments['name'])));
 
         return sprintf(
-            '<div class="image-preview-wrapper">
-                <img class="image-preview" src="%s" width="100" height="100">
-            </div>
-            <input type="button" class="button upload-media-button" value="%s" data-attachment-id="%s">
-            <input type="hidden" name="%s" value="%s">',
+            '<div class="media-upload-input">
+                <div class="image-preview-wrapper">
+                    <img class="image-preview" src="%s" width="100" height="100" style="border: 1px #ccc solid;">
+                </div>
+                <input type="button" class="button upload-media-button" value="%s">
+                <a href="#" class="clear-media-button" style="margin-left: 20px; vertical-align: sub;">%s</a>
+                <input type="hidden" name="%s" value="%s">
+            </div>',
             $currentMediaUrl,
             __('Select media', 'wordclass'),
-            get_option($arguments['name']),
+            __('Clear', 'wordclass'),
             $arguments['name'],
             get_option($arguments['name'])
         );
