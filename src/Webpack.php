f21b3b96d4f01733cf0029a5bf3c901b3cc8a4be
Sybren Dijkstra <nerbiz@gmail.com>
2019-11-20 20:59:28 +0100

    Webpack is now completely static, renamed methods

--- src/Webpack.php
+++ src/Webpack.php
@@ -2,6 +2,8 @@
 
 namespace Nerbiz\Wordclass;
 
+use Exception;
+
 class Webpack
 {
     /**
@@ -10,52 +12,46 @@ class Webpack
      */
     protected static $manifest;
 
-    public function __construct()
-    {
-        if (static::$manifest === null) {
-            static::$manifest = (object)[];
-        }
-    }
-
     /**
-     * Parse a manifest file
-     * @param  string|null $path The full path to manifest.json, default path if null
-     * @return self
-     * @throws \Exception If the path is not readable
+     * Read a manifest file
+     * @param  string|null $path The full path to the manifest file, default path if null
+     * @return void
+     * @throws Exception If the path is not readable
      */
-    public function parse(?string $path = null): self
+    public static function readManifest(?string $path = null): void
     {
         if ($path === null) {
             $path = get_stylesheet_directory() . '/dist/manifest.json';
         }
 
         if (! is_readable($path)) {
-            throw new \Exception(sprintf(
+            throw new Exception(sprintf(
                 "%s(): the manifest.json path '%s' is not readable",
                 __METHOD__,
                 is_object($path) ? get_class($path) : $path
             ));
         }
 
-        // Set the parsed manifest
+        // Read the manifest
         static::$manifest = json_decode(file_get_contents($path));
-
-        return $this;
     }
 
     /**
      * Get an asset URL by the original name
      * @param  string $originalFilename
      * @return string|null
+     * @throws Exception If the asset is not found
      */
-    public static function getAsset(string $originalFilename): ?string
+    public static function getAssetUrl(string $originalFilename): ?string
     {
-        if (property_exists(static::$manifest, $originalFilename)) {
+        if (isset(static::$manifest->{$originalFilename})) {
             return static::$manifest->{$originalFilename};
         }
 
-        // Trigger a 404 error if not found
-        http_response_code(404);
-        return null;
+        throw new \Exception(sprintf(
+            "%s(): asset '%s' not found in the manifest",
+            __METHOD__,
+            $originalFilename
+        ));
     }
 }
