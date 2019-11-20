bfc1c50399d752510c95daa578676e9aa3222b23
Sybren Dijkstra <nerbiz@gmail.com>
2019-11-20 21:21:56 +0100

    Yoast::addBreadcrumb() now supports PostType objects

--- src/Yoast.php
+++ src/Yoast.php
@@ -6,21 +6,25 @@ class Yoast
 {
     /**
      * Add a custom breadcrumb
-     * @param string $postType The post type to add a breadcrumb to
-     * @param int    $pageId The ID of the page in the breadcrumb link
-     * @param int    $offset The location of the breadcrumb
+     * @param string|PostType $postType The post type to add a breadcrumb to
+     * @param int             $postId   The ID of the post in the breadcrumb link
+     * @param int             $offset   The location of the breadcrumb
      * @return self
      */
-    public function addBreadcrumb(string $postType, int $pageId, int $offset = -1): self
+    public function addBreadcrumb($postType, int $postId, int $offset = -1): self
     {
-        add_filter('wpseo_breadcrumb_links', function ($links) use ($postType, $pageId, $offset) {
+        add_filter('wpseo_breadcrumb_links', function ($links) use ($postType, $postId, $offset) {
+            if ($postType instanceof PostType) {
+                $postType = $postType->getId();
+            }
+
             // Adjust the breadcrumbs for the posts
             if (is_singular($postType)) {
                 // Add the custom breadcrumb
                 array_splice($links, $offset, 0, [
                     [
-                        'url'  => get_permalink($pageId),
-                        'text' => get_the_title($pageId),
+                        'url'  => get_permalink($postId),
+                        'text' => get_the_title($postId),
                     ]
                 ]);
             }
