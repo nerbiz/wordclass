4d4bd908aab7ee85fb884d753d851d75ca81c597
Sybren Dijkstra <nerbiz@gmail.com>
2020-08-01 15:21:36 +0200

    Added an example for implementing in functions.php

---
+++ example/classes/YourPostTypesTaxonomies.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace NewProject;
+
+use Nerbiz\Wordclass\PostType;
+use Nerbiz\Wordclass\Taxonomy;
+
+class YourPostTypesTaxonomies
+{
+    /**
+     * @return PostType
+     */
+    public function createCalendarItemPostType(): PostType
+    {
+        return (new PostType('calendar_item'))
+            ->setSingularName(__('Calendar item', 'project-text-domain'))
+            ->setPluralName(__('Calendar items', 'project-text-domain'))
+            ->setSupports(['title', 'editor', 'thumbnail'])
+            ->setArguments([
+                'public' => true,
+                'publicly_queryable' => true,
+                // Enable Gutenberg editor
+                'show_in_rest' => true,
+            ])
+            ->register();
+    }
+
+    /**
+     * @param PostType $postType
+     * @return Taxonomy
+     */
+    public function createCalendarItemTagTaxonomy(PostType $postType): Taxonomy
+    {
+        return (new Taxonomy('calendar_item_tag'))
+            ->setSingularName(__('Calendar item tag', 'project-text-domain'))
+            ->setPluralName(__('Calendar item tags', 'project-text-domain'))
+            ->setPostTypes([$postType])
+            ->setArguments([
+                // Enable on Gutenberg edit page
+                'show_in_rest' => true,
+            ])
+            ->register();
+    }
+}
---
+++ example/functions.php
@@ -0,0 +1,111 @@
+<?php
+
+use Nerbiz\Wordclass\Admin;
+use Nerbiz\Wordclass\Assets;
+use Nerbiz\Wordclass\Editor;
+use Nerbiz\Wordclass\Init;
+use Nerbiz\Wordclass\Media;
+use Nerbiz\Wordclass\Pages;
+use Nerbiz\Wordclass\Theme;
+use Nerbiz\Wordclass\Webpack;
+// Example namespace, autoloaded from composer.json
+use NewProject\YourPostTypesTaxonomies;
+use NewProject\YourSettingsPage;
+
+require get_stylesheet_directory() . '/vendor/autoload.php';
+
+// Initial settings and values
+Init::setPrefix('aaa');
+(new Init())->setTimezone()
+    ->loadTranslations()
+    ->defineConstants()
+    ->includeHelperFunctions();
+
+// This path is the default,
+// only shown here to illustrate that you can set a custom path
+$manifestPath = get_stylesheet_directory() . '/dist/manifest.json';
+Webpack::readManifest($manifestPath);
+
+(new Assets())
+    // Set a custom jQuery version (front-end only)
+    ->jQueryVersion('3.5.1')
+    // Or remove it (front-end only)
+    ->removeJquery()
+    // Add some assets to your pages
+    ->addThemeCss('theme-front', get_stylesheet_uri() . '/dist/style.css')
+    ->addThemeJs('theme-front', 'https://example.com/script.js')
+    ->addAdminCss('theme-admin', Webpack::getAssetUrl('admin.css'))
+    ->addAdminJs('theme-admin', Webpack::getAssetUrl('admin.js'))
+    ->addLoginCss('theme-login', Webpack::getAssetUrl('login.css'))
+    ->addLoginJs('theme-login', Webpack::getAssetUrl('login.js'));
+
+// Set some theme options
+(new Theme())
+    // Don't show WordPress version information (for security)
+    ->removeGeneratorMeta()
+    ->enableFeaturedImages()
+    ->enableHtml5Support()
+    ->addMenus([
+        'primary' => __('Main menu', 'project-text-domain'),
+        'footer' => __('Footer menu', 'project-text-domain'),
+    ]);
+
+(new Media())
+    ->setFeaturedImageSize(1024, 768)
+    // Add some useful image sizes
+    ->addImageSize('card', 'Card', 500, 500, false)
+    ->addImageSize('maximum', 'Maximum', 1200, 1200, false)
+    // Allow SVG files in the media library
+    ->addUploadSupport('svg', 'image/svg+xml');
+
+(new Pages())
+    ->automaticWindowTitle()
+    // The part between site and page title, e.g. 'About us - Company name'
+    // HTML is supported
+    ->setWindowTitleSeparator('&middot;')
+    // Disable search functionality if you wish
+    ->disableSearchFunctionality()
+    // Set page titles for 404 and search result pages
+    ->set404PageTitle('Not found')
+    ->setSearchPageTitle('Search results');
+
+// Login and admin options
+global $wp_version;
+(new Admin())
+    // Show a custom footer text in admin
+    ->footerText(sprintf(
+        '%s - <em>%s</em> (Wordpress v%s)',
+        get_bloginfo('name'),
+        get_bloginfo('description'),
+        $wp_version
+    ))
+    // Redirect users after they login (by role)
+    ->roleRedirect('editor', home_url('welcome'))
+    // Add an admin bar button,
+    // Which can move it to the bottom of the page
+    ->addMoveAdminBarButton();
+
+// Create a custom settings page
+// Moved to a separate class, to not pollute the functions.php file too much
+(new YourSettingsPage())->create();
+
+// Create post types and/or taxonomies
+// Moved to a separate class, to not pollute the functions.php file too much
+$postTypesTaxonomies = new YourPostTypesTaxonomies();
+$cptCalendarItem = $postTypesTaxonomies->createCalendarItemPostType();
+$postTypesTaxonomies->createCalendarItemTagTaxonomy($cptCalendarItem);
+
+// Adjust the TinyMCE editor (if you're not using Gutenberg)
+(new Editor())
+    ->removeH1FromFormats()
+    ->forceAdvanced()
+    // Add a 'bold' button on the second toolbar,
+    // after the 'indent' button
+    ->addButton('bold', 'indent', 2)
+    // Or remove a button
+    ->removeButton('bold')
+    // Replace the 'bold' button with an 'indent' button
+    ->replaceButton('bold', 'indent')
+    // Add a 'table' button on the second toolbar,
+    // after the 'indent' button
+    ->addPlugin('table', 'indent', 2);
