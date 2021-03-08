3cb61839fbb62118e414c814951b93561886bd92
Sybren Dijkstra <nerbiz@gmail.com>
2021-03-09 00:43:01 +0100

    Converted media input code to normale JS, added a general file icon

--- includes/html/media-input-field.php
+++ includes/html/media-input-field.php
@@ -1,18 +1,39 @@
 <?php
+
+use Nerbiz\WordClass\Init;
+
 if (! isset($currentMediaUrl, $currentMediaFilename, $prefixedName, $inputValue)) {
     return;
 }
 
 $noMediaSelectedText = __('No media selected', 'wordclass');
+
+$imagesDirectory = Init::getVendorUri('nerbiz/wordclass/includes/images/');
+$transparentPixelSrc = $imagesDirectory . 'transparent-pixel.png';
+// Public domain icon downloaded from
+// https://publicdomainvectors.org/en/free-clipart/Paper-sheet-vector-image/13299.html
+$fileIconSrc = $imagesDirectory . 'file-icon.svg';
+
+// Set a fallback image, if the current URL is empty
+if (trim($currentMediaUrl) === '') {
+    if ($currentMediaFilename !== '') {
+        // If a filename is set, use the general file icon
+        $currentMediaUrl = $fileIconSrc;
+    } else {
+        $currentMediaUrl = $transparentPixelSrc;
+    }
+}
 ?>
 
-<div class="media-upload-input">
+<div class="wc-media-upload-input">
     <div class="media-preview-wrapper">
         <img class="media-preview"
              src="<?php echo $currentMediaUrl; ?>"
              width="100"
              height="100"
-             style="border: 1px #ccc solid;">
+             style="border: 1px #ccc solid;"
+             data-transparent-pixel-src="<?php echo $transparentPixelSrc; ?>"
+             data-file-icon-src="<?php echo $fileIconSrc; ?>">
     </div>
 
     <input type="button"
