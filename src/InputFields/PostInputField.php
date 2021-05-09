4b8636e4e0e80ef324a77ca113862fce15abb7f1
Sybren Dijkstra <nerbiz@gmail.com>
2021-05-10 01:22:31 +0200

    PostInputField now extends SelectInputField

--- src/InputFields/PostInputField.php
+++ src/InputFields/PostInputField.php
@@ -4,7 +4,7 @@ namespace Nerbiz\WordClass\InputFields;
 
 use WP_Post;
 
-class PostInputField extends AbstractInputField
+class PostInputField extends SelectInputField
 {
     /**
      * The post types to select from
@@ -25,99 +25,46 @@ class PostInputField extends AbstractInputField
         array $postTypes = []
     ) {
         $this->postTypes = $postTypes;
+        $values = $this->createValuesArray();
 
-        parent::__construct($name, $label, $description);
+        parent::__construct($name, $label, $description, $values);
     }
 
     /**
-     * {@inheritdoc}
+     * Create an array for the select options
+     * @return array
      */
-    public function renderField(): string
+    public function createValuesArray(): array
     {
         // Get all the posts
         $allPosts = get_posts([
-            'post_type' => $this->getPostTypes(),
+            'post_type' => (count($this->postTypes) > 0)
+                ? $this->postTypes
+                : array_merge(
+                    ['post', 'page'],
+                    array_values(get_post_types(['_builtin' => false]))
+                ),
             'post_status' => 'publish',
             'posts_per_page' => -1,
             'orderby' => 'post_type',
             'order' => 'asc',
         ]);
 
-        $groupsString = array_reduce(
-            $this->getDropdownPostGroups($allPosts),
-            function ($current, $groupProperties) {
-                // Create option elements
-                $optionsString = array_reduce($groupProperties['posts'], function ($current, $post) {
-                    return $current . sprintf(
-                        '<option value="%s" %s>%s</option>',
-                        $post->ID,
-                        selected($post->ID, get_option($this->getPrefixedName()), false),
-                        $post->post_title
-                    );
-                }, '');
-
-                // Create an optgroup with options
-                return $current . sprintf(
-                    '<optgroup label="%s">
-                        %s
-                    </optgroup>',
-                    $groupProperties['label'],
-                    $optionsString
-                );
-            },
-            ''
-        );
-
-        return sprintf(
-            '<select name="%s">
-                <option value="">%s</option>
-                %s
-            </select>',
-            $this->getPrefixedName(),
-            __('- Choose one -', 'wordclass'),
-            $groupsString
-        );
-    }
-
-    /**
-     * See which post types to use for the posts query
-     * @return array
-     */
-    protected function getPostTypes(): array
-    {
-        if (count($this->postTypes) > 0) {
-            return $this->postTypes;
-        }
-
-        // If no type(s) are given, use all post types (except built-in)
         return array_merge(
-            ['post', 'page'],
-            array_values(get_post_types(['_builtin' => false]))
-        );
-    }
-
-    /**
-     * Create an array in preparation for constructing the dropdown options
-     * @param WP_Post[] $posts
-     * @return array
-     */
-    protected function getDropdownPostGroups(array $posts): array
-    {
-        return array_reduce($posts, function ($current, $post) {
-            // Create the post type key if it doesn't exist yet
-            if (! array_key_exists($post->post_type, $current)) {
+            ['' => __('- Choose one -', 'wordclass')],
+            array_reduce($allPosts, function (array $current, WP_Post $post) {
                 $postTypeObject = get_post_type_object($post->post_type);
 
-                $current[$post->post_type] = [
-                    'label' => $postTypeObject->label,
-                    'posts' => [],
-                ];
-            }
+                // Create the post type key if it doesn't exist yet
+                if (! array_key_exists($postTypeObject->label, $current)) {
+                    $current[$postTypeObject->label] = [];
+                }
 
-            // Add the post to the list
-            $current[$post->post_type]['posts'][] = $post;
+                // Add the post to the list
+                $current[$postTypeObject->label][$post->ID] = $post->post_title;
 
-            return $current;
-        }, []);
+                return $current;
+            }, [])
+        );
     }
 }
