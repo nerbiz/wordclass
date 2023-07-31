3d1db5218d1b9058e90e723c9b31d44f45c65374
Sybren Dijkstra <nerbiz@gmail.com>
2023-08-01 01:15:57 +0200

    Updated the example YourSettingsPage class

--- example/classes/YourSettingsPage.php
+++ example/classes/YourSettingsPage.php
@@ -2,9 +2,15 @@
 
 namespace NewProject;
 
-use Nerbiz\WordClass\InputFields\MediaInputField;
-use Nerbiz\WordClass\InputFields\RadioButtonsInputField;
+use Nerbiz\WordClass\InputFields\CheckboxInputField;
+use Nerbiz\WordClass\InputFields\EditorInputField;
+use Nerbiz\WordClass\InputFields\ExplanationText;
 use Nerbiz\WordClass\InputFields\GeneralInputField;
+use Nerbiz\WordClass\InputFields\MediaInputField;
+use Nerbiz\WordClass\InputFields\PostInputField;
+use Nerbiz\WordClass\InputFields\RadioButtonsInputField;
+use Nerbiz\WordClass\InputFields\SelectInputField;
+use Nerbiz\WordClass\InputFields\TextareaInputField;
 use Nerbiz\WordClass\SettingsPage;
 use Nerbiz\WordClass\SettingsPageSection;
 
@@ -13,7 +19,7 @@ class YourSettingsPage
     /**
      * @var SettingsPage
      */
-    protected $settingsPage;
+    protected SettingsPage $settingsPage;
 
     /**
      * Create the settings page
@@ -21,9 +27,12 @@ class YourSettingsPage
      */
     public function create(): void
     {
-        $settingsPageTitle = __('Website settings', 'project-text-domain');
+        /**
+         * This class doesn't use the translate function, for brevity.
+         * Of course you can replace 'Some text' with __('Some text', 'project-text-domain').
+         */
 
-        $this->settingsPage = (new SettingsPage($settingsPageTitle))
+        $this->settingsPage = (new SettingsPage('Website settings'))
             // Make it a submenu of the normal WordPress settings
             ->setParentSlug('options-general.php')
             // Allow editors to use this settings page
@@ -31,8 +40,9 @@ class YourSettingsPage
             ->setCapabilityByRole('editor');
 
         $this->addContactSection();
-        $this->addSocialMediaSection();
+        $this->addTestSection();
 
+        // Don't forget this
         $this->settingsPage->create();
     }
 
@@ -41,37 +51,84 @@ class YourSettingsPage
      */
     protected function addContactSection(): void
     {
-        $section = new SettingsPageSection('contact', __('Contact', 'project-text-domain'), null, [
-            (new MediaInputField('logo_id', __('Logo', 'project-text-domain')))
-                // All fields support an optional description
-                ->setDescription(__('The company logo', 'project-text-domain')),
-            new GeneralInputField('company', __('Company name', 'project-text-domain')),
-            new GeneralInputField('street', __('Address', 'project-text-domain')),
-            new GeneralInputField('postcode', __('Postcode', 'project-text-domain')),
-            new GeneralInputField('city', __('City', 'project-text-domain')),
-            new GeneralInputField('phone', __('Phone number', 'project-text-domain')),
-            new GeneralInputField('email', __('Email address', 'project-text-domain')),
-            new GeneralInputField('vat_number', __('VAT number', 'project-text-domain')),
-            // Radio button fields expect an array of values, for each radio button
-            (new RadioButtonsInputField('show_map', __('Show map', 'project-text-domain'), [
-                '1' => __('Yes', 'project-text-domain'),
-                '0' => __('No', 'project-text-domain'),
-            ]))->setDescription(__('Whether to show the map on the contact page', 'project-text-domain')),
+        // Each section needs a unique ID, title and input fields
+        $section = new SettingsPageSection('contact', 'Contact', [
+            // It's possible to add subheadings with extra descriptive text
+            (new ExplanationText('Contact information settings'))
+                ->setDescription('Several values to be shown on the contact page'),
+            // Some general input fields with type="text"
+            new GeneralInputField('street', 'Street and house number'),
+            new GeneralInputField('postcode', 'Postcode'),
+            new GeneralInputField('city', 'City'),
+            (new GeneralInputField('phone', 'Phone number'))
+                // Some useful hint for the input
+                ->setDescription('Format: 123-456-7890')
+                // Any attribute can be set
+                ->setAttributes([
+                    'type' => 'tel',
+                    'pattern' => '[0-9]{3}-[0-9]{3}-[0-9]{4}',
+                ]),
+            (new GeneralInputField('email', 'Email address'))
+                ->setAttributes(['type' => 'email']),
         ]);
 
         $this->settingsPage->addSection($section);
     }
 
     /**
+     * This section features all possible input fields for a complete example
      * @return void
      */
-    protected function addSocialMediaSection(): void
+    protected function addTestSection(): void
     {
-        $section = new SettingsPageSection('social', __('Social media', 'project-text-domain'), null, [
-            new GeneralInputField('facebook_url', __('Facebook', 'project-text-domain')),
-            new GeneralInputField('instagram_url', __('Instagram', 'project-text-domain')),
-            new GeneralInputField('twitter_url', __('Twitter', 'project-text-domain')),
-            new GeneralInputField('linkedin_url', __('LinkedIn', 'project-text-domain')),
+        /**
+         * This is a more elaborate example,
+         * showing all possible input elements
+         */
+
+        $section = new SettingsPageSection('general', 'General', [
+            (new GeneralInputField('company_name', 'Company name'))
+                // All input fields support an optional description
+                ->setDescription('The name of the company'),
+            (new GeneralInputField('max_news_items', 'Maximum amount of news items'))
+                ->setDescription('Referring to the items on the homepage')
+                ->setAttributes([
+                    'type' => 'number',
+                    'min' => 3,
+                    'max' => 9,
+                    // Boolean attributes only need the name
+                    'required',
+                    // data-* attributes are possible too
+                    'data-test-value' => 'something',
+                ]),
+            // A textarea field for more content than a normal <input>
+            (new TextareaInputField('vacation_message', 'Vacation message'))
+                ->setDescription('A short message to show during the vacation period'),
+            // A text editor for rich text
+            (new EditorInputField('footer_content', 'Footer content'))
+                ->setDescription('Company description on each page in the footer'),
+            // An input using the WordPress media library
+            new MediaInputField('logo_id', 'Logo'),
+            // A subheading can be placed anywhere in between
+            new ExplanationText('Some settings with predefined options'),
+            // Checkboxes and radio buttons for simple or predefined settings
+            new CheckboxInputField('show_map', 'Show a map on the contact page'),
+            // Radio button fields expect an array of value=>label pairs
+            new RadioButtonsInputField('map_type', 'The type of map to show', [
+                'roadmap' => 'Default map with roads',
+                'satellite' => 'A map with images from satellites',
+                'terrain' => 'A map featuring terrain information',
+            ]),
+            // A dropdown element, which expects an array of value=>label pairs
+            new SelectInputField('country', 'Country', [
+                'country_1' => 'Country 1',
+                'country_2' => 'Country 2',
+                'country_3' => 'Country 3',
+            ]),
+            // A posts dropdown, to choose an ID of a specific post
+            // PostType objects are also supported
+            (new PostInputField('promotion_page', 'Promotion page', ['post', 'page']))
+                ->setDescription('Where a promo button could link to'),
         ]);
 
         $this->settingsPage->addSection($section);
